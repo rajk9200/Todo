c676f107e62bc617a2068ef3346eddce5b3ec87d
rajk9200 <rajkushwaha1111@gmail.com>
2024-08-10 16:33:20 +0530

    todo project uploaded

---
+++ python_question.py
@@ -0,0 +1,35 @@
+# #code editor for python
+
+#Question 1
+file_1 =['Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red'] #8
+file_2 = ['White', 'White', 'White', 'White', 'White', 'White', 'White', 'White', 'White', 'White', 'White'] #11
+# output ["Red","White","Red",........"White","Red","White","White","White"]
+
+#Question 2
+a= ['a','b','b','c','a']
+# output a2b2c1
+
+#
+# without count function
+s = "abbbbcca"
+# # output 2a4b2c
+
+
+#Question 3 Sorting list when int and str both
+inp= [1,'asw3',4,'fdd453po','6756dsfsd']
+# output ['wsa3', 'ssfdd7665', 'pofdd543', 4, 1]
+
+#Question 4 Sorting Json Data in python by id
+json_data =[
+    {'id':1,'title':"honesty is best policy.","status":True},
+    {'id':3,'title':"Helth is welth.","status":True},
+    {'id': 2, 'title': "Enjoying in Manali.", "status": False},
+    {'id': 5, 'title': "Iphone 15 lunched.", "status": False},
+    {'id': 4, 'title': "Sumsung S23 cemera is better then Iphone.", "status": False},
+]
+#output  arrange description data by id
+
+# Question 5 remove blank element from list
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+#output ['Mike', 'Emma', 'Kelly', 'Brad']
+
